180d8c616703d51e570ff4337031f5ef80c4025c
robertrusu <rusu.robertt@gmail.com>
2021-07-22 22:43:01 +0300

    added comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import ReactModal from 'react-modal';
 Modal.setAppElement('#root');
 
-
+///Styles for modal
 const customStyles = {
   content: {
     top: "50%",
@@ -26,7 +26,6 @@ class App extends React.Component {
       modalIsOn: false,
       
     }
-    
     this.handleCloseModal = this.handleCloseModal.bind(this); 
   }
 
@@ -34,7 +33,7 @@ class App extends React.Component {
 
 
   
- 
+    ///API call for separate info about crypto and changing state for modal opening 
     handleClick = (id) => {
     console.log(`you clicked on ${id}`)
     fetch(`https://api.coingecko.com/api/v3/coins//${id}?localization=true&per_page=10`)
@@ -47,7 +46,7 @@ class App extends React.Component {
     });
     }
     
-  
+    ///Closing modal 
     handleCloseModal() {
       this.setState({
         modalIsOn: false,
@@ -72,151 +71,152 @@ class App extends React.Component {
       
       render () {
         var {info, idCrypto } = this.state;
-      return (
-        <div className="App">
-        
-        <table className="table is-bordered is-fullwidth">
-          <tr>
-            <th  scope="col" className="">Crypto Name</th>
-            
-            {info.map(elem => (
-                <th scope="col" className="container">
-                <div className="container has-text-centered">
-                
-                <h1>
-                  {elem.name}
-                </h1>
-                
-                  
-                    
-                  
-                  <div className="has-text-centered container">
-                <button
-                className="button is-primary is-light is-small" 
-                onClick = {() => {this.handleClick(elem.id)}}>More Info</button>
-                <div id="modal" className="modal">
+        return (
+                  <div className="App">
+                    {/*Table*/}
+                    <table className="table is-bordered is-fullwidth">
+                      <tr>
+                        <th  scope="col" className="">Crypto Name</th>
+                        {/*Map Function for rendering info */}
+                            {info.map(elem => (
+                                <th scope="col" className="container">
+                                  <div className="container has-text-centered">
+                                
+                                    <h1>
+                                      {elem.name}
+                                    </h1>
+                                
+                                  
+                                    
+                                  
+                                    <div className="has-text-centered container">
+                                      {/*Opening Modal Button*/}
+                                      <button
+                                        className="button is-primary is-light is-small" 
+                                        onClick = {() => {this.handleClick(elem.id)}}>More Info</button>
+                                          <div id="modal" className="modal">
 
-                  
-                        <ReactModal
-                        isOpen={this.state.modalIsOn}
-                        contentLabel="Minimal Modal Example"
-                        style={customStyles}>
-                  <div className="modal-content has-text-centered">
+                                            {/*Modal Component*/}
+                                              <ReactModal
+                                              isOpen={this.state.modalIsOn}
+                                              contentLabel="Minimal Modal Example"
+                                              style={customStyles}>
+                                                <div className="modal-content has-text-centered">
+                                              
+                                                  <h1 className="subtitle is-6">Name:{idCrypto.name}</h1><br/>
+                                                  <h1 className="subtitle is-6">Symbol:{idCrypto.symbol}</h1><br/>
+                                                  <h1 className="subtitle is-6">Hashing algorithm:{idCrypto.hashing_algorithm}</h1><br/>
+                                                  <h1 className="subtitle is-6">Description:{idCrypto.description?.en}</h1><br/>
+                                                  <h1 className="subtitle is-6">Market cap in euro:{idCrypto.market_cap_change_24h_in_currency?.eur}</h1><br/>
+                                                  <h1 className="subtitle is-6">Homepage:{idCrypto.links?.homepage}</h1><br/>
+                                                  <h1 className="subtitle is-6">Genesis Date:{idCrypto.genesis_date}</h1><br/>
+                                                    <br/>
+                                                    {/*Closing Modal Button*/}
+                                                    <button className="button is-primary is-light is-small"
+                                                    onClick = {() => {this.handleCloseModal()}}>Close Info</button>
+                                                </div>
+                                                </ReactModal>
+                                            </div>
+
+                                  
                         
-                          <h1 className="subtitle is-6">Name:{idCrypto.name}</h1><br/>
-                          <h1 className="subtitle is-6">Symbol:{idCrypto.symbol}</h1><br/>
-                          <h1 className="subtitle is-6">Hashing algorithm:{idCrypto.hashing_algorithm}</h1><br/>
-                          <h1 className="subtitle is-6">Description:{idCrypto.description?.en}</h1><br/>
-                          <h1 className="subtitle is-6">Market cap in euro:{idCrypto.market_cap_change_24h_in_currency?.eur}</h1><br/>
-                          <h1 className="subtitle is-6">Homepage:{idCrypto.links?.homepage}</h1><br/>
-                          <h1 className="subtitle is-6">Genesis Date:{idCrypto.genesis_date}</h1><br/>
-                        <br/>
-                        <button className="button is-primary is-light is-small"
-                        onClick = {() => {this.handleCloseModal()}}>Close Info</button>
-                  </div>
-                        </ReactModal>
-                </div>
-
-                  
-        
-                
-                </div>
-                </div>
-            {/*    
-                    {(this.state.modalIsOn ? <Modal/> : this.hideModal())}
-            */}
-                    
-                    </th>
-                    
-                    ))}
-                    
+                                
+                                    </div>
+                                  </div>
+                            
+                                    
+                                </th>
+                                    
+                                    ))}
+                                    
+                                      
                           
-              
-            
-            
-          </tr>
-          <tr>
-              <th className="">
-                  Crypto Image
-              </th>
+                        
+                        
+                      </tr>
+                      {/*Tr for Crypto Images*/}
+                      <tr>
+                          <th className="">
+                              Crypto Image
+                          </th>
 
 
-              {info.map(elem =>(
-                  <th>
-                    <div className="container">
-                      <img key={elem.image} src={elem.image}/>
-                    </div> 
-                  </th>
-                ))}
-          </tr>
-
-          <tr>
-              <th className="">
-              
-                  Crypto Symbol
-               
-              </th>
-
-
-              {info.map(elem =>(
-                  <th>
-                    <div className="container">
-                      {elem.symbol}
-                    </div> 
-                  </th>
-                ))}
-          </tr>
-
-          <tr>
-              <th className="">
-                  Current Price
-              </th>
+                          {info.map(elem =>(
+                              <th>
+                                <div className="container">
+                                  <img key={elem.image} src={elem.image}/>
+                                </div> 
+                              </th>
+                            ))}
+                      </tr>
+                      {/*Tr for Crypto Symbol*/}
+                      <tr>
+                          <th className="">
+                          
+                              Crypto Symbol
+                          
+                          </th>
 
 
-              {info.map(elem =>(
-                  <th>
-                    <div className="container">
-                      {elem.current_price}
-                    </div> 
-                  </th>
-                ))}
-          </tr>
-
-          <tr>
-              <th className="">
-                  High 24 Hour Price
-              </th>
-
-
-              {info.map(elem =>(
-                  <th>
-                    <div className="container">
-                      {elem.high_24h}
-                    </div> 
-                  </th>
-                ))}
-          </tr>
-
-          <tr>
-              <th className="">
-                  Low 24 Hour Price
-              </th>
+                          {info.map(elem =>(
+                              <th>
+                                <div className="container">
+                                  {elem.symbol}
+                                </div> 
+                              </th>
+                            ))}
+                      </tr>
+                      {/*Tr For Crypto current prices*/}
+                      <tr>
+                          <th className="">
+                              Current Price
+                          </th>
 
 
-              {info.map(elem =>(
-                  <th>
-                    <div className="container">
-                      {elem.low_24h}
-                    </div> 
-                  </th>
-                ))}
-          </tr>
-        
-        </table>
-      
-    </div>
-  );
-    }
+                          {info.map(elem =>(
+                              <th>
+                                <div className="container">
+                                  {elem.current_price}
+                                </div> 
+                              </th>
+                            ))}
+                      </tr>
+                      {/*Tr for Highes Price in the last 24h*/}
+                      <tr>
+                          <th className="">
+                              High 24 Hour Price
+                          </th>
+
+
+                          {info.map(elem =>(
+                              <th>
+                                <div className="container">
+                                  {elem.high_24h}
+                                </div> 
+                              </th>
+                            ))}
+                      </tr>
+                      {/*Tr for Lowest Price in the last 24h*/}
+                      <tr>
+                          <th className="">
+                              Low 24 Hour Price
+                          </th>
+
+
+                          {info.map(elem =>(
+                              <th>
+                                <div className="container">
+                                  {elem.low_24h}
+                                </div> 
+                              </th>
+                            ))}
+                      </tr>
+                    
+                    </table>
+                
+              </div>
+            );
+              }
   }
 
 export default App;
